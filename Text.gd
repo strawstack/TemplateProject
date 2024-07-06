eea412f97daa07069fc13109f4e4c7f9f4207784
RichardHayes <rhshadowgreen@gmail.com>
2024-07-05 20:53:16 -0700

    basic text box engine

--- Text.gd
+++ Text.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 @export var charSpeed = 0.03
 @export var pauseSpeed = 0.2
-@export var chunkLimit = 240
+@export var chunkLimit = 210
 
 # Dialogue is a list of data items
 var dialogueIndex = 0
@@ -23,6 +23,7 @@ var charAnimationInProgress = false
 
 # Stores the current dialogue
 var full_dialogue = null
+var currentChunks = null
 var currentText = null
 
 # Time value since last event
@@ -31,6 +32,7 @@ var timer = 0
 var textBox = null
 var box = null
 var textLabel = null
+var nextLabel = null
 
 var punctuation = ".,?!:"
 
@@ -39,17 +41,21 @@ func _ready():
 	textBox = get_parent()
 	box = textBox.get_node("Box")
 	textLabel = textBox.get_node("Box/RichTextLabel")
+	nextLabel = textBox.get_node("Box/NextLabel")
 	
 	# Set initial visibility
 	box.visible = false
+	nextLabel.visible = false
 
 func _process(delta):
 	if dialogueInProgress:
 		if charAnimationInProgress:
+			nextLabel.visible = false
 			# On action key press, complete current line of dialogue immediatly
 			if Input.is_action_just_pressed("action"):
 				setVisibleCharacters(currentText.length())
 				charAnimationInProgress = false
+				maybeAddThreeDots()
 
 			else: # Advance the timer by one frame
 				timer += delta
@@ -61,14 +67,28 @@ func _process(delta):
 				if timer >= timeLimit:
 					timer = 0
 					setVisibleCharacters(charIndex + 1)
-					
 					if charIndex == currentText.length():
 						charAnimationInProgress = false
-						print("charAnimationInProgress = false")
+						maybeAddThreeDots()
+
 		else:
-			# We get here when dialogue is in progress and characters are not animating
-			# Thus an action key press should advance to the next segment or dialogue index
-			pass
+			nextLabel.visible = true
+			if Input.is_action_just_pressed("action"):
+				# Advance segment
+				segmentIndex += 1
+
+				# Go to next segment
+				if segmentIndex == currentChunks.size():
+					segmentIndex = 0
+					dialogueIndex += 1
+
+				# End dialogue
+				if dialogueIndex == full_dialogue.size():
+					dialogueInProgress = false
+					box.visible = false
+
+				else:
+					displayData(full_dialogue[dialogueIndex])
 
 func reset():
 	dialogueIndex = 0
@@ -86,8 +106,8 @@ func beginDialogue():
 	displayData(full_dialogue[dialogueIndex])
 
 func displayData(data):
-	var text = chunkText(data["text"])
-	currentText = text[segmentIndex]
+	currentChunks = chunkText(data["text"])
+	currentText = currentChunks[segmentIndex]
 	
 	# Configure text to be 0 chars showing
 	setVisibleCharacters(0)
@@ -105,13 +125,31 @@ func chunkText(text):
 	if text.length() <= chunkLimit:
 		return [text]
 	else:
+		var rtn = []
 		var words = text.split(" ")
+		var total = 0
+		var chunk = []
 		for word in words:
-			print(word)
+			if word.length() + 1 + total >= chunkLimit:
+				rtn.push_back(" ".join(chunk))
+				chunk.clear()
+				chunk.push_back(word)
+				total = word.length() + 1
+			else:
+				chunk.push_back(word)
+				total += word.length() + 1
+		if chunk.size() > 0:
+			rtn.push_back(" ".join(chunk))
+		return rtn
 
 func setVisibleCharacters(value):
 	charIndex = value
 	textLabel.set_visible_characters(value)
+
+func maybeAddThreeDots():
+	if segmentIndex < currentChunks.size() - 1:
+		textLabel.set_text(textLabel.get_text() + "...")
+		setVisibleCharacters(charIndex + 3)
 
 func _on_body_entered(body):
 	beginDialogue()
